d3f4ddd39b8f17f17cf4f6e25211689dce9c3ec6
hubery.wang <hbrwang@outlook.com>
2020-08-06 23:18:12 +0800

    edit

--- Hubery.Tools.Uwp/HbrUwpResources.cs
+++ Hubery.Tools.Uwp/HbrUwpResources.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Hubery.Tools.Uwp.Helpers;
+using Microsoft.Toolkit.Uwp.UI.Helpers;
+using System;
+using Windows.UI;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
 
 namespace Hubery.Tools.Uwp
 {
@@ -26,6 +26,13 @@ namespace Hubery.Tools.Uwp
             {
                 MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri(path, UriKind.RelativeOrAbsolute) });
             }
+
+            new ThemeListener().ThemeChanged += OnThemeChanged;
+        }
+
+        private void OnThemeChanged(ThemeListener sender)
+        {
+            Application.Current.Resources["ThemeForeground"] = Helpers.ColorHelper.IsDarkColor(ResourcesHelper.GetResource<Color>("SystemAccentColor")) ? new SolidColorBrush(Colors.White) : new SolidColorBrush(Colors.Black);
         }
     }
 }
