32d5425e4fbe9deda2e5afdc378ef91d2a54c811
hal-wang <hi@hal.wang>
2021-01-05 14:51:50 +0800

    add uwp test

---
+++ UwpTest/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using HTools;
+using HTools.Uwp.Helpers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UwpTest
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await TaskExtend.SleepAsync(1000);
+            await PopupHelper.ShowDialog("Test", "Title", "OK", "Cancel");
+        }
+    }
+}
