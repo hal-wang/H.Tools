db3f6cdd83edd8d6fc8cd60eb73f2ac71e664758
hal-wang <hi@hal.wang>
2021-08-25 16:22:17 +0800

    feat: NumCompareConverter

--- Converters/NumCompareConverter.cs
+++ Converters/NumCompareConverter.cs
@@ -9,10 +9,8 @@ using System.Globalization;
 using Windows.UI.Xaml.Data;
 #endif
 
-namespace HTools.Converters
-{
-    internal class NumCompareConverter : IValueConverter
-    {
+namespace HTools.Converters {
+    internal class NumCompareConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter,
 #if NET452
             CultureInfo language
@@ -20,18 +18,78 @@ namespace HTools.Converters
 #if UAP10_0_18362
             string language
 #endif
-            )
-        {
+            ) {
             string param = parameter as string;
-            var isTrue = (param[0]) switch
-            {
-                '>' => (int)value > int.Parse(param[1].ToString()),
-                '<' => (int)value < int.Parse(param[1].ToString()),
-                '=' => (int)value == int.Parse(param[1].ToString()),
-                _ => throw new Exception(),
-            };
+            return ConverterHelper.BoolTo(CompareNumber(param[0], param.Remove(0, 1), value), targetType);
+        }
 
-            return ConverterHelper.BoolTo(isTrue, targetType);
+        private bool CompareNumber(char ch, string compareValue, object value) {
+            if (value is short shortNum) {
+                return ch switch {
+                    '>' => shortNum > short.Parse(compareValue),
+                    '<' => shortNum < short.Parse(compareValue),
+                    '=' => shortNum == short.Parse(compareValue),
+                    _ => throw new NotSupportedException(),
+                };
+            } else if (value is ushort ushortNum) {
+                return ch switch {
+                    '>' => ushortNum > ushort.Parse(compareValue),
+                    '<' => ushortNum < ushort.Parse(compareValue),
+                    '=' => ushortNum == ushort.Parse(compareValue),
+                    _ => throw new NotSupportedException(),
+                };
+            } else if (value is int intNum) {
+                return ch switch {
+                    '>' => intNum > int.Parse(compareValue),
+                    '<' => intNum < int.Parse(compareValue),
+                    '=' => intNum == int.Parse(compareValue),
+                    _ => throw new NotSupportedException(),
+                };
+            } else if (value is uint uintNum) {
+                return ch switch {
+                    '>' => uintNum > uint.Parse(compareValue),
+                    '<' => uintNum < uint.Parse(compareValue),
+                    '=' => uintNum == uint.Parse(compareValue),
+                    _ => throw new NotSupportedException(),
+                };
+            } else if (value is long longNum) {
+                return ch switch {
+                    '>' => longNum > long.Parse(compareValue),
+                    '<' => longNum < long.Parse(compareValue),
+                    '=' => longNum == long.Parse(compareValue),
+                    _ => throw new NotSupportedException(),
+                };
+            } else if (value is ulong ulongNum) {
+                return ch switch {
+                    '>' => ulongNum > ulong.Parse(compareValue),
+                    '<' => ulongNum < ulong.Parse(compareValue),
+                    '=' => ulongNum == ulong.Parse(compareValue),
+                    _ => throw new NotSupportedException(),
+                };
+            } else if (value is float floutNum) {
+                return ch switch {
+                    '>' => floutNum > float.Parse(compareValue),
+                    '<' => floutNum < float.Parse(compareValue),
+                    '=' => floutNum == float.Parse(compareValue),
+                    _ => throw new NotSupportedException(),
+                };
+            } else if (value is double doubleNum) {
+                return ch switch {
+                    '>' => doubleNum > double.Parse(compareValue),
+                    '<' => doubleNum < double.Parse(compareValue),
+                    '=' => doubleNum == double.Parse(compareValue),
+                    _ => throw new NotSupportedException(),
+                };
+            } else if (value is decimal decimalNum) {
+                return ch switch {
+                    '>' => decimalNum > decimal.Parse(compareValue),
+                    '<' => decimalNum < decimal.Parse(compareValue),
+                    '=' => decimalNum == decimal.Parse(compareValue),
+                    _ => throw new NotSupportedException(),
+                };
+            } else {
+                throw new NotSupportedException();
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter,
@@ -41,8 +99,7 @@ namespace HTools.Converters
 #if UAP10_0_18362
             string language
 #endif
-            )
-        {
+            ) {
             throw new NotImplementedException();
         }
     }
