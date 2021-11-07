9c7c1f5bd31362b3865851c5b8f552cffe8648d5
hal-wang <hi@hal.wang>
2021-11-07 22:33:06 +0800

    merge

--- Config/SqliteConfigBase.cs
+++ Config/SqliteConfigBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.IO;
 using System.Linq;
 
@@ -6,9 +7,14 @@ namespace HTools.Config
 {
     public abstract class SqliteConfigBase : ConfigBase<string>, IDisposable
     {
-        private SqliteBase<ConfigItem> _sqliteConnection;
+        private SQLiteConnection _con;
 
         public SqliteConfigBase() { }
+
+        public SqliteConfigBase(SQLiteConnection con)
+        {
+            _con = con;
+        }
 
         public SqliteConfigBase(string path)
         {
@@ -17,20 +23,20 @@ namespace HTools.Config
 
         public void Connect(string path)
         {
-            _sqliteConnection?.Dispose();
-            _sqliteConnection = new SqliteBase<ConfigItem>(path ?? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "config.db"));
+            _con?.Dispose();
+            _con = new SqliteBase<ConfigItem>(path ?? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "config.db"));
         }
 
-        public override bool ContainsKey(string key) => _sqliteConnection.Table<ConfigItem>().Any(item => item.Key == key);
+        public override bool ContainsKey(string key) => _con.Table<ConfigItem>().Any(item => item.Key == key);
 
         protected override string GetValue(string key = null)
         {
-            return _sqliteConnection.Find<ConfigItem>(key).Value;
+            return _con.Find<ConfigItem>(key).Value;
         }
 
         protected override void SetValue(string value, string key)
         {
-            _sqliteConnection.InsertOrReplace(new ConfigItem()
+            _con.InsertOrReplace(new ConfigItem()
             {
                 Key = key,
                 Value = value
@@ -39,12 +45,12 @@ namespace HTools.Config
 
         public override void Remove(string key)
         {
-            _sqliteConnection.Table<ConfigItem>().Where(item => item.Key == key).Delete();
+            _con.Table<ConfigItem>().Where(item => item.Key == key).Delete();
         }
 
         public void Dispose()
         {
-            _sqliteConnection.Dispose();
+            _con.Dispose();
         }
     }
 }
