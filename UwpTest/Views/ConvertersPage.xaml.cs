356f644098437a8d3b191e325e26b36699edb32d
hal-wang <hi@hal.wang>
2023-02-11 21:43:33 +0800

    feat: add FormatStringConverter

---
+++ UwpTest/Views/ConvertersPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UwpTest.Views
+{
+    public sealed partial class ConvertersPage : Page
+    {
+        public ConvertersPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public string TestString
+        {
+            get { return (string)GetValue(TestStringProperty); }
+            set { SetValue(TestStringProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TestString.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TestStringProperty =
+            DependencyProperty.Register("TestString", typeof(string), typeof(ConvertersPage), new PropertyMetadata("test"));
+    }
+}
