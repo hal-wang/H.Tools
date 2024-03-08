f8bd4e6267b7ee5db0c7fdbd35c37034a2614ab7
hal-wang <hi@hal.wang>
2024-03-08 22:36:15 +0800

    feat(data): extend nameReplace

--- src/H.Tools.Data/SqlDataTable.cs
+++ src/H.Tools.Data/SqlDataTable.cs
@@ -1,43 +1,112 @@
-﻿using System;
+﻿using OneOf;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Dynamic;
+using System.Reflection;
 using System.Xml.Linq;
 
 namespace H.Tools.Data;
 
 public static class SqlDataTable
 {
-    public static List<T> ToList<T>(this DataTable table) where T : class, new()
+    public static List<T> ToList<T>(this DataTable table, Func<string, OneOf<string, string[]>> nameReplace = null) where T : class, new()
     {
         var result = new List<T>();
-        var propertys = typeof(T).GetProperties();
+        var properties = typeof(T).GetProperties();
         foreach (DataRow dr in table.Rows)
         {
-            var item = new T();
+            var item = dr.ToObject<T>(properties, nameReplace);
             result.Add(item);
-            foreach (var pi in propertys)
-            {
-                if (!table.Columns.Contains(pi.Name)) continue;
-                var value = dr[pi.Name];
-                if (value is DBNull || value == null) continue;
-                pi.SetValue(item, value);
-            }
         }
         return result;
     }
 
-    public static List<dynamic> ToList(this DataTable table)
+    public static T ToObject<T>(this DataRow row, Func<string, OneOf<string, string[]>> nameReplace = null) where T : class, new()
+    {
+        var properties = typeof(T).GetProperties();
+        return row.ToObject<T>(properties, nameReplace);
+    }
+
+    private static string GetName(this DataColumnCollection columns, string name)
+    {
+        if (columns.Contains(name))
+        {
+            return name;
+        }
+        else
+        {
+            foreach (DataColumn col in columns)
+            {
+                if (string.Equals(col.ColumnName, name, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return col.ColumnName;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static string GetName(this DataColumnCollection columns, string name, Func<string, OneOf<string, string[]>> nameReplace)
+    {
+        var replaceNames = nameReplace(name);
+        if (replaceNames.IsT0)
+        {
+            var result = columns.GetName(replaceNames.AsT0);
+            if (!string.IsNullOrEmpty(name))
+            {
+                return result;
+            }
+        }
+        else
+        {
+            foreach (var replaceName in replaceNames.AsT1)
+            {
+                var result = columns.GetName(replaceName);
+                if (!string.IsNullOrEmpty(name))
+                {
+                    return result;
+                }
+            }
+        }
+        return columns.GetName(name);
+    }
+
+    private static T ToObject<T>(this DataRow row, PropertyInfo[] properties, Func<string, OneOf<string, string[]>> nameReplace = null) where T : class, new()
+    {
+        var result = new T();
+        foreach (var pi in properties)
+        {
+            var name = row.Table.Columns.GetName(pi.Name, nameReplace);
+            if (string.IsNullOrEmpty(name)) continue;
+
+            var value = row[name];
+            if (value is DBNull || value == null) continue;
+            pi.SetValue(result, value);
+        }
+        return result;
+    }
+
+    public static List<dynamic> ToList(this DataTable table, Func<string, OneOf<string, string[]>> nameReplace = null)
     {
         var result = new List<dynamic>();
         foreach (DataRow dr in table.Rows)
         {
-            dynamic item = new ExpandoObject();
-            for (var i = 0; i < table.Columns.Count; i++)
-            {
-                ((IDictionary<string, object>)item).Add(table.Columns[i].ColumnName, dr[i] == DBNull.Value ? null : dr[i]);
-            }
+            dynamic item = dr.ToObject(nameReplace);
             result.Add(item);
+        }
+        return result;
+    }
+
+    public static dynamic ToObject(this DataRow row, Func<string, OneOf<string, string[]>> nameReplace = null)
+    {
+        dynamic result = new ExpandoObject();
+        for (var i = 0; i < row.Table.Columns.Count; i++)
+        {
+            var name = row.Table.Columns.GetName(row.Table.Columns[i].ColumnName, nameReplace);
+            if (string.IsNullOrEmpty(name)) continue;
+
+            ((IDictionary<string, object>)result).Add(name, row[i] == DBNull.Value ? null : row[i]);
         }
         return result;
     }
