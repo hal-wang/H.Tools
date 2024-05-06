283db44b1ed66ac1cd59ba6ac62cfb8af96835a8
hal-wang <hi@hal.wang>
2024-05-06 17:16:15 +0800

    feat(config): support for custom section

--- src/H.Tools.Config/AppSettingsConfiguration.cs
+++ src/H.Tools.Config/AppSettingsConfiguration.cs
@@ -5,24 +5,46 @@ namespace H.Tools.Config;
 
 public class AppSettingsConfiguration : Configuration
 {
-    public override bool ContainsKey(string key) => ConfigurationManager.AppSettings.AllKeys.Contains(key);
+    public override bool ContainsKey(string key) => GetSection(GetConfiguration()).Settings.AllKeys.Contains(key);
 
-    protected override string GetValue(string key) => ConfigurationManager.AppSettings[key];
+    protected override string GetValue(string key) => GetSection(GetConfiguration()).Settings[key]?.Value;
 
     protected override void SetValue(string value, string key)
     {
-        System.Configuration.Configuration cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-        cfa.AppSettings.Settings.Remove(key);
-        cfa.AppSettings.Settings.Add(key, value);
-        cfa.Save(ConfigurationSaveMode.Modified);
-        ConfigurationManager.RefreshSection("appSettings");
+        var config = GetConfiguration();
+        var section = GetSection(config);
+        section.Settings.Remove(key);
+        section.Settings.Add(key, value);
+        config.Save(ConfigurationSaveMode.Modified);
+        ConfigurationManager.RefreshSection(SectionName);
     }
 
     public override void Remove(string key)
     {
-        System.Configuration.Configuration cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-        cfa.AppSettings.Settings.Remove(key);
-        cfa.Save(ConfigurationSaveMode.Modified);
-        ConfigurationManager.RefreshSection("appSettings");
+        var config = GetConfiguration();
+        var section = GetSection(config);
+        section.Settings.Remove(key);
+        config.Save(ConfigurationSaveMode.Modified);
+        ConfigurationManager.RefreshSection(SectionName);
+    }
+
+    private AppSettingsSection GetSection(System.Configuration.Configuration config)
+    {
+        var section = (AppSettingsSection)config.GetSection(SectionName);
+        if (section == null)
+        {
+            section = new AppSettingsSection();
+            config.Sections.Add(SectionName, section);
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(SectionName);
+        }
+        return section;
+    }
+
+    protected virtual string SectionName { get; } = "AppSettings";
+
+    protected virtual System.Configuration.Configuration GetConfiguration()
+    {
+        return ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
     }
 }
