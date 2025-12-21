b878fd02fa682b106a28bc52914a8363b75e4ef4
hal-wang <hi@hal.wang>
2025-12-21 11:40:41 +0800

    feat(Wpf): update ClickToFocusBehaviour

--- src/H.Tools.Wpf/Behaviors/ClickToFocusBehaviour.cs
+++ src/H.Tools.Wpf/Behaviors/ClickToFocusBehaviour.cs
@@ -18,7 +18,90 @@ public sealed class ClickToFocusBehaviour : Behavior<UIElement>
 
     private static void OnTargetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        (d as ClickToFocusBehaviour)!.SetFocusVisualStyle();
+        if (d is not ClickToFocusBehaviour behaviour) return;
+
+        behaviour.SetFocusVisualStyle();
+    }
+
+    public bool PreviewMouseLeftButtonDownEnable
+    {
+        get { return (bool)GetValue(PreviewMouseLeftButtonDownEnableProperty); }
+        set { SetValue(PreviewMouseLeftButtonDownEnableProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for PreviewMouseLeftButtonDownEnable.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty PreviewMouseLeftButtonDownEnableProperty =
+        DependencyProperty.Register(nameof(PreviewMouseLeftButtonDownEnable), typeof(bool), typeof(ClickToFocusBehaviour), new PropertyMetadata(true, OnEnableChanged));
+
+
+    public bool PreviewMouseLeftButtonUpEnable
+    {
+        get { return (bool)GetValue(PreviewMouseLeftButtonUpEnableProperty); }
+        set { SetValue(PreviewMouseLeftButtonUpEnableProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for PreviewMouseLeftButtonUpEnable.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty PreviewMouseLeftButtonUpEnableProperty =
+        DependencyProperty.Register(nameof(PreviewMouseLeftButtonUpEnable), typeof(bool), typeof(ClickToFocusBehaviour), new PropertyMetadata(false, OnEnableChanged));
+
+
+    public bool MouseLeftButtonDownEnable
+    {
+        get { return (bool)GetValue(MouseLeftButtonDownEnableProperty); }
+        set { SetValue(MouseLeftButtonDownEnableProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for MouseLeftButtonDownEnable.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty MouseLeftButtonDownEnableProperty =
+        DependencyProperty.Register(nameof(MouseLeftButtonDownEnable), typeof(bool), typeof(ClickToFocusBehaviour), new PropertyMetadata(false, OnEnableChanged));
+
+
+    public bool MouseLeftButtonUpEnable
+    {
+        get { return (bool)GetValue(MouseLeftButtonUpEnableProperty); }
+        set { SetValue(MouseLeftButtonUpEnableProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for MouseLeftButtonUpEnable.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty MouseLeftButtonUpEnableProperty =
+        DependencyProperty.Register(nameof(MouseLeftButtonUpEnable), typeof(bool), typeof(ClickToFocusBehaviour), new PropertyMetadata(true, OnEnableChanged));
+
+
+    private static void OnEnableChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not ClickToFocusBehaviour behaviour) return;
+
+        behaviour.AttachEvents();
+    }
+
+    private void AttachEvents()
+    {
+        DetachEvents();
+
+        if (PreviewMouseLeftButtonDownEnable)
+        {
+            AssociatedObject.PreviewMouseLeftButtonDown += SetFacusEvent;
+        }
+        if (PreviewMouseLeftButtonUpEnable)
+        {
+            AssociatedObject.PreviewMouseLeftButtonUp += SetFacusEvent;
+        }
+        if (MouseLeftButtonDownEnable)
+        {
+            AssociatedObject.MouseLeftButtonDown += SetFacusEvent;
+        }
+        if (MouseLeftButtonUpEnable)
+        {
+            AssociatedObject.MouseLeftButtonUp += SetFacusEvent;
+        }
+    }
+
+    private void DetachEvents()
+    {
+        AssociatedObject.PreviewMouseLeftButtonDown -= SetFacusEvent;
+        AssociatedObject.PreviewMouseLeftButtonUp -= SetFacusEvent;
+        AssociatedObject.MouseLeftButtonDown -= SetFacusEvent;
+        AssociatedObject.MouseLeftButtonUp -= SetFacusEvent;
     }
 
     protected override void OnAttached()
@@ -26,14 +109,14 @@ public sealed class ClickToFocusBehaviour : Behavior<UIElement>
         base.OnAttached();
 
         SetFocusVisualStyle();
-        AssociatedObject.PreviewMouseLeftButtonDown += PreviewMouseLeftButtonDown;
+        AttachEvents();
     }
 
     protected override void OnDetaching()
     {
         base.OnDetaching();
 
-        AssociatedObject.PreviewMouseLeftButtonDown -= PreviewMouseLeftButtonDown;
+        DetachEvents();
     }
 
     private void SetFocusVisualStyle()
@@ -43,7 +126,7 @@ public sealed class ClickToFocusBehaviour : Behavior<UIElement>
         fe.FocusVisualStyle = null;
     }
 
-    private void PreviewMouseLeftButtonDown(object? sender, MouseButtonEventArgs eventArgs)
+    private void SetFacusEvent(object? sender, MouseButtonEventArgs eventArgs)
     {
         Target?.Focus();
     }
