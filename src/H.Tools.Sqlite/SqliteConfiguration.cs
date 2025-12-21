61f498209a45c6922f38cc49214952c3bb3b6994
hal-wang <hi@hal.wang>
2025-12-21 11:35:21 +0800

    feat(Sqlite): use Microsoft.Data.Sqlite

--- src/H.Tools.Sqlite/SqliteConfiguration.cs
+++ src/H.Tools.Sqlite/SqliteConfiguration.cs
@@ -1,59 +1,57 @@
 ﻿using H.Tools.Config;
-using SQLite;
+using H.Tools.Data;
+using Microsoft.Data.Sqlite;
 using System;
-using System.IO;
-using System.Linq;
 
 namespace H.Tools.Sqlite;
 
-[Table("Configuration")]
-file class ConfigItem
-{
-    [PrimaryKey]
-    public string Key { get; set; }
-    public string Value { get; set; }
-}
-
 public class SqliteConfiguration : Configuration, IDisposable
 {
-    private SQLiteConnection Connection => _initCon ?? _cbCon();
+    private SqliteConnection Connection => _initCon ?? (_cbCon ??= _conCb());
 
-    private readonly SQLiteConnection _initCon;
-    public SqliteConfiguration(SQLiteConnection con)
+    private readonly SqliteConnection _initCon;
+    public SqliteConfiguration(SqliteConnection con)
     {
         _initCon = con;
+        Init();
     }
 
-    private readonly Func<SQLiteConnection> _cbCon;
-    public SqliteConfiguration(Func<SQLiteConnection> con)
+    private SqliteConnection _cbCon;
+    private readonly Func<SqliteConnection> _conCb;
+    public SqliteConfiguration(Func<SqliteConnection> con)
     {
-        _cbCon = con;
+        _conCb = con;
+        Init();
     }
 
-    public SqliteConfiguration(string path = null) : this(() => new SingleSqlite<ConfigItem>(path ?? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.db")))
+    public SqliteConfiguration(string path = "./config.db") : this(() => new SqliteConnection($"Data Source={path};Cache=2"))
     {
     }
 
-    public override bool ContainsKey(string key) => Connection.Table<ConfigItem>().Any(item => item.Key == key);
+    private void Init()
+    {
+        if (Connection.State != System.Data.ConnectionState.Open)
+        {
+            Connection.OpenAsync().Wait();
+        }
+        Connection.ExecuteNonQueryAsync("CREATE TABLE IF NOT EXISTS Configuration (Key TEXT PRIMARY KEY, Value TEXT);").Wait();
+    }
+
+    public override bool ContainsKey(string key) => Connection.ExecuteScalarAsync<int>($"SELECT COUNT(*) FROM Configuration WHERE Key = '{key.Replace("'", "''")}';").Result > 0;
 
     protected override string GetValue(string key = null)
     {
-        return Connection.Find<ConfigItem>(key).Value;
+        return Connection.ExecuteScalarAsync<string>($"SELECT Value FROM Configuration WHERE Key = '{key.Replace("'", "''")}';").Result ?? "";
     }
 
     protected override void SetValue(string value, string key)
     {
-        Connection.InsertOrReplace(new ConfigItem()
-        {
-            Key = key,
-            Value = value
-        });
+        Connection.ExecuteNonQueryAsync($"INSERT OR REPLACE INTO Configuration (Key, Value) VALUES ('{key.Replace("'", "''")}', '{value.Replace("'", "''")}');").Wait();
     }
 
-    public override void Remove(string key)
+    protected override void RemoveKey(string key)
     {
-        Connection.Table<ConfigItem>().Where(item => item.Key == key).Delete();
-        base.Remove(key);
+        Connection.ExecuteNonQueryAsync($"DELETE FROM Configuration WHERE Key = '{key.Replace("'", "''")}';").Wait();
     }
 
     public void Dispose()
